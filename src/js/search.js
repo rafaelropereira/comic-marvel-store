f179a27fe478b5eae2d3bd59106f745e3801acfe
Rafael Pereira <rafael.ropereira@gmail.com>
2019-02-13 00:31:27 -0200

    Formatação inicial da tela de busca - estrutura base em desenvolvimento.

--- src/js/search.js
+++ src/js/search.js
@@ -2,6 +2,7 @@ import MarvelAPI from "./marvelAPI.js";
 
 // Criando uma nova instância da marvelAPI onde estão todos os métodos necessários para popular os dados e a interface.
 const marvelAPI = new MarvelAPI();
+marvelAPI.disableBackdrop();
 
 // Selecionando os elementos do DOM
 const inputSearch = document.getElementById('inputSearch');
